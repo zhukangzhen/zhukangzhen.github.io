1c3e31a0b6a226977bdafe73cfb0e17bd26283d3
zhukangzhen <1652061423@qq.com>
2020-12-31 13:08:14 +0800

    修改vuex

--- src/vuex/mutations.js
+++ src/vuex/mutations.js
@@ -16,7 +16,11 @@ export const SET_LOAD_STATUS = (state, status) => {
   state.isLoading = status;
 };
 
-
+// 设置秒杀商品
+export const SET_SECKILLS_INFO = (state, seckills) => {
+  state.seckills.goodsList = seckills[0];
+  state.seckills.deadline = seckills[1];
+};
 // 设置轮播(营销)图
 export const SET_CAROUSELITEMS_INFO = (state, { carouselItems, activity }) => {
   state.marketing.CarouselItems = carouselItems;
